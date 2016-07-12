8d80165917544316992e9d455f78ad9ee48157e3
Ximin Luo <infinity0@debian.org>
2016-07-12 13:10:28 +0200

    Update add-soname patch, set rustc build-dep version in update-version.sh

--- debian/update-version.sh
+++ debian/update-version.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-ORIG=1.9
-NEW=1.10
+update() {
+local OLD=$1 ORIG=$2 NEW=$3
 
-sed -i -e "s|libstd-rust-$ORIG|libstd-rust-$NEW|g" control
+sed -i -e "s|libstd-rust-$ORIG|libstd-rust-$NEW|g" \
+       -e "s|rustc (>= $OLD|rustc (>= $ORIG|g" control
 
 git mv libstd-rust-$ORIG.lintian-overrides libstd-rust-$NEW.lintian-overrides
 sed -i -e "s|libstd-rust-$ORIG|libstd-rust-$NEW|g" libstd-rust-$NEW.lintian-overrides
+}
 
+update 1.8 1.9 1.10
