c5fe2a1de3a458d1ec363e4a5386b831c7f89c85
Ximin Luo <infinity0@debian.org>
2016-07-16 01:44:06 +0200

    Don't allow rebuilds by newer versions

--- debian/update-version.sh
+++ debian/update-version.sh
@@ -3,6 +3,7 @@ update() {
 local OLD=$1 ORIG=$2 NEW=$3
 
 sed -i -e "s|libstd-rust-$ORIG|libstd-rust-$NEW|g" \
+       -e "s|rustc (<= $ORIG|rustc (<= $NEW|g" \
        -e "s|rustc (>= $OLD|rustc (>= $ORIG|g" control
 
 git mv libstd-rust-$ORIG.lintian-overrides libstd-rust-$NEW.lintian-overrides
