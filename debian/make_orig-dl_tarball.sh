427dce5b1dad42ad86847bc86e9eb678011b1cb9
Ximin Luo <infinity0@debian.org>
2016-07-16 19:35:15 +0200

    Add a source_orig-dl target to help bootstrap new distros
    - also make make_orig-dl_tarball.sh more reproducible
    - update documentation about our new bootstrapping processes
    - rename dlstage0 build profile to fit the "pkg.$package.$profilename" schema.
      lintian still complains, never mind..

--- debian/make_orig-dl_tarball.sh
+++ debian/make_orig-dl_tarball.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
-# For versions 1.10 and later.
-# ONLY FOR BOOTSTRAPPING with DEB_BUILD_PROFILES=dlstage0
+# See README.Debian "Bootstrapping a new distro" for details.
 #
-# Will create rustc_1.10.0+dfsg1.orig-dl.tar.xz; you should include this
-# with your source upload.
+# You should probably use `debian/rules source_orig-dl` instead of calling this
+# directly.
 
 set -e
 
 upstream_version="$(dpkg-parsechangelog -SVersion | sed -e 's/\(.*\)-.*/\1/g')"
 supported_arch="x86_64 i686"
 
-rm -rf dl/*.sha256
+rm -f dl/*.sha256
 for arch in $supported_arch; do
 	triplet="${arch}-unknown-linux-gnu"
 	python src/etc/get-stage0.py "$triplet"
+	rm -rf "$triplet"
 done
 
 tar --mtime=@"$(date +%s)" --clamp-mtime \
+  --owner=root --group=root \
   -cJf "../rustc_${upstream_version}.orig-dl.tar.xz" \
   --transform "s/^dl\///" \
   dl/*
+
+rm -f src/bootstrap/bootstrap.pyc
