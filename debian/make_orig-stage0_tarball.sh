75be2bb3447c0d4ac70681d8ac914be4e5860ad3
Ximin Luo <infinity0@debian.org>
2017-05-31 15:57:51 +0200

    Fix bootstrapping and rename the tarball to orig-stage0

--- debian/make_orig-stage0_tarball.sh
+++ debian/make_orig-stage0_tarball.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# See README.Debian "Bootstrapping a new distro" for details.
+# See README.Debian "Bootstrapping" for details.
 #
-# You may want to use `debian/rules source_orig-dl` instead of calling this
+# You may want to use `debian/rules source_orig-stage0` instead of calling this
 # directly.
 
 set -e
@@ -9,31 +9,32 @@ set -e
 upstream_version="$(dpkg-parsechangelog -SVersion | sed -e 's/\(.*\)-.*/\1/g')"
 upstream_bootstrap_arch="${upstream_bootstrap_arch:-amd64 arm64 armel armhf i386 powerpc ppc64el s390x}"
 
-rm -f dl/*.sha256
+rm -f stage0/*/*.sha256
+mkdir build && ln -sf ../stage0 build/cache
 for deb_host_arch in $upstream_bootstrap_arch; do
 	make -s --no-print-directory -f debian/architecture-test.mk "rust-for-deb_${deb_host_arch}" | {
 	read deb_host_arch rust_triplet
-	python src/etc/get-stage0.py "${rust_triplet}"
+	PYTHONPATH=src/bootstrap debian/get-stage0.py "${rust_triplet}"
 	rm -rf "${rust_triplet}"
 	}
 done
 
-tar --mtime=@"$(date +%s)" --clamp-mtime \
+tar --mtime=@"${SOURCE_DATE_EPOCH:-$(date +%s)}" --clamp-mtime \
   --owner=root --group=root \
-  -cJf "../rustc_${upstream_version}.orig-dl.tar.xz" \
-  --transform "s/^dl\///" \
-  dl/*
+  -cJf "../rustc_${upstream_version}.orig-stage0.tar.xz" \
+  --transform "s/^stage0\///" \
+  stage0/*
 
 rm -f src/bootstrap/bootstrap.pyc
 
 cat <<eof
 ================================================================================
-orig-dl bootstrapping tarball created in ../rustc_${upstream_version}.orig-dl.tar.xz
+orig-stage0 bootstrapping tarball created in ../rustc_${upstream_version}.orig-stage0.tar.xz
 containing the upstream compilers for $upstream_bootstrap_arch
 
 You *probably* now want to do the following steps:
 
-1. Add [$(echo $upstream_bootstrap_arch | sed -e 's/\S*/!\0/g')] to the rustc Build-Depend in d/control
+1. Add [$(echo $upstream_bootstrap_arch | sed -e 's/\S*/!\0/g')] to the rustc/cargo Build-Depends in d/control
 2. Update d/changelog
 3. Run \`dpkg-source -b .\` to generate a .dsc that includes this tarball.
 ================================================================================
